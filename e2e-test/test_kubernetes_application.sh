538f38458888acfb53de41490b5b24f24c246d26
wangyang0918 <danrtsey.wy@alibaba-inc.com>
2022-01-25 13:26:57 +0800

    Update example to use Flink K8s HA

--- e2e-test/test_kubernetes_application.sh
+++ e2e-test/test_kubernetes_application.sh
@@ -21,38 +21,35 @@ CLUSTER_ID="flink-example-statemachine"
 TIMEOUT=300
 TEST_NAMESPACE="flink-test"
 
-function check_logs_output {
-  local pod_name=$1
-  local successful_response_regex=$2
-  LOG_CONTENT=$(kubectl -n ${TEST_NAMESPACE} logs $pod_name 2> /dev/null)
-
-  # ensure the log content adapts with the successful regex
-  if [[ ${LOG_CONTENT} =~ ${successful_response_regex} ]]; then
-    return 0
-  fi
-  return 1
-}
-
 function wait_for_logs {
   local jm_pod_name=$1
   local successful_response_regex=$2
-  local timeout=${3:-${TIMEOUT}}
-
-  echo "Waiting for jobmanager pod ${jm_pod_name} ready."
-  kubectl -n ${TEST_NAMESPACE} wait --for=condition=Ready --timeout=${timeout}s pod/$jm_pod_name || exit 1
 
   # wait or timeout until the log shows up
   echo "Waiting for log \"$2\"..."
-  for i in $(seq 1 ${timeout}); do
-    if check_logs_output $jm_pod_name $successful_response_regex; then
+  for i in $(seq 1 ${TIMEOUT}); do
+    if kubectl -n ${TEST_NAMESPACE} logs $jm_pod_name | grep -E "${successful_response_regex}" >/dev/null; then
       echo "Log \"$2\" shows up."
       return
     fi
 
     sleep 1
   done
-  echo "Log $2 does not show up within a timeout of ${timeout} sec"
+  echo "Log $2 does not show up within a timeout of ${TIMEOUT} sec"
   exit 1
+}
+
+function cleanup_and_exit() {
+    local exit_code=${1:-"0"}
+
+    kubectl -n ${TEST_NAMESPACE} delete -f deploy/cr.yaml
+    kubectl -n ${TEST_NAMESPACE} delete -f deploy/flink-rbac.yaml
+
+    kubectl delete -f deploy/flink-native-k8s-operator.yaml
+    kubectl delete -f deploy/crd.yaml
+    kubectl delete ns ${TEST_NAMESPACE}
+
+    exit $exit_code
 }
 
 kubectl create ns ${TEST_NAMESPACE}
@@ -74,19 +71,26 @@ for i in $(seq 1 $TIMEOUT);do
   sleep 1
 done
 
-kubectl -n ${TEST_NAMESPACE} wait --for=condition=Available --timeout=${TIMEOUT}s deploy/${CLUSTER_ID} || exit 1
+kubectl -n ${TEST_NAMESPACE} wait --for=condition=Available --timeout=${TIMEOUT}s deploy/${CLUSTER_ID} || cleanup_and_exit 1
 jm_pod_name=$(kubectl -n ${TEST_NAMESPACE} get pods --selector="app=${CLUSTER_ID},component=jobmanager" -o jsonpath='{..metadata.name}')
+
+echo "Waiting for jobmanager pod ${jm_pod_name} ready."
+kubectl -n ${TEST_NAMESPACE} wait --for=condition=Ready --timeout=${TIMEOUT}s pod/$jm_pod_name || cleanup_and_exit 1
 
 wait_for_logs $jm_pod_name "Rest endpoint listening at"
 
-wait_for_logs $jm_pod_name "Completed checkpoint 1 for job"
+wait_for_logs $jm_pod_name "Completed checkpoint [0-9]+ for job"
 
-kubectl -n ${TEST_NAMESPACE} delete -f deploy/cr.yaml
-kubectl -n ${TEST_NAMESPACE} wait --for=delete --timeout=${TIMEOUT}s deploy/${CLUSTER_ID} || exit 1
-kubectl -n ${TEST_NAMESPACE} delete -f deploy/flink-rbac.yaml
+job_id=$(kubectl -n ${TEST_NAMESPACE} logs $jm_pod_name | grep -E -o 'Job [a-z0-9]+ is submitted' | awk '{print $2}')
 
-kubectl delete -f deploy/flink-native-k8s-operator.yaml
-kubectl delete -f deploy/crd.yaml
-kubectl delete ns ${TEST_NAMESPACE}
+# Kill the JobManager
+echo "Kill the $jm_pod_name"
+kubectl -n ${TEST_NAMESPACE} exec $jm_pod_name -- /bin/sh -c "kill 1" || cleanup_and_exit 1
+
+# Check the new JobManager recovering from latest successful checkpoint
+wait_for_logs $jm_pod_name "Restoring job $job_id from Checkpoint"
+wait_for_logs $jm_pod_name "Completed checkpoint [0-9]+ for job"
 
 echo "Successfully run the Flink Kubernetes application test"
+
+cleanup_and_exit
